d62860905b5927d399cf320b55b0df68ca85fadf
Dr. James Burke <burkej@sbregional.org>
2024-03-22 12:20:46 -0400

    Added goofball

--- main.ts
+++ main.ts
@@ -17,6 +17,17 @@ let aDuck = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(aDuck)
+let Goofball = sprites.create(img`
+    . . b b b b . . 
+    . b 5 5 5 5 b . 
+    b 5 d 3 3 d 5 b 
+    b 5 3 5 5 1 5 b 
+    c 5 3 5 5 1 d c 
+    c d d 1 1 d d c 
+    . f d d d d f . 
+    . . f f f f . . 
+    `, SpriteKind.Player)
+Goofball.setPosition(31, 90)
 game.onUpdate(function () {
     if (aDuck.vx < 0) {
         aDuck.setImage(assets.image`myImage1`)
